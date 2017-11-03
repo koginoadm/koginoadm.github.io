a3ca8277f41d79651765a3ec3073786e4f618317
koginoadm <31156607+koginoadm@users.noreply.github.com>
2017-11-04 06:21:16 +0900

    Create awscli.sh

---
+++ awscli.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+if [[ "$(\grep 'Ubuntu 16.04' /etc/lsb-release 2>/dev/null)" ]]
+then
+    apt update
+    apt upgrade -y
+    apt install python-pip -y
+fi
+#
+if [[ "$(\grep 'CentOS Linux release 7' /etc/centos-release 2>/dev/null)" ]]
+    yum install python-devel -y
+    curl -LR --connect-timeout 10 https://bootstrap.pypa.io/get-pip.py | python
+fi
+#
+pip install awscli
