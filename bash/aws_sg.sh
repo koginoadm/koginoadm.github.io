a6ba0ad1de850f65e02b83924eb9a064186ed64d
koginoadm <31156607+koginoadm@users.noreply.github.com>
2017-11-05 03:41:37 +0900

    Update and rename aws-sg.sh to aws_sg.sh

--- bash/aws_sg.sh
+++ bash/aws_sg.sh
@@ -1,11 +1,14 @@
-#!/bin/bash -e
-# aws-sg.sh
+#!/bin/bash
+# aws_sg.sh
+#
 # aws ec2 describe-security-groups | jq -r .SecurityGroups[].GroupId
 # aws ec2 describe-security-groups --group-id sg-xxxxxxxx
 #
-vCurrentIp="$(curl -LRsS http://checkip.amazonaws.com/ | \egrep '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')"
-vGroupId="sg-xxxxxxxx"
-vFile="${HOME:?}/${vGroupId}"
+. /usr/local/bin/function_mail.sh
+#
+declare vGroupId="sg-xxxxxxxx"
+declare vCurrentIp="$(curl -LRsS http://checkip.amazonaws.com/ | \egrep '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')"
+declare vFile="${HOME:?}/${vGroupId}"
 #
 if [[ "$(< ${vFile})" != "${vCurrentIp:?}" ]]
 then
@@ -16,7 +19,9 @@ then
         aws ec2 revoke-security-group-ingress --group-id ${vGroupId} --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "'"$(< ${vFile})"'/32", "Description": "d1.djeeno.net"}]}]'
     fi
     #
-    echo "${vCurrentIp}" > "${vFile}"
+    printf "${vCurrentIp}" > "${vFile}"
     #aws ec2 authorize-security-group-ingress --group-id ${vGroupId} --protocol tcp --port 22 --cidr $(< ${vFile})/32
     aws ec2 authorize-security-group-ingress --group-id ${vGroupId} --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "'"$(< ${vFile})"'/32", "Description": "d1.djeeno.net"}]}]'
+    mailbash koginoadm@outlook.com "[AWS][SG][d1.djeeno.net] IP Address was changed." '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "'"$(< ${vFile})"'/32", "Description": "d1.djeeno.net"}]}]'
 fi
+
