e0130ba27dcf1b189ebf768fec2cf69011883a76
koginoadm <31156607+koginoadm@users.noreply.github.com>
2017-11-05 03:49:15 +0900

    Update aws_sg.sh

--- bash/aws_sg.sh
+++ bash/aws_sg.sh
@@ -7,7 +7,7 @@
 . /usr/local/bin/function_mail.sh
 #
 declare vGroupId="sg-xxxxxxxx"
-declare vCurrentIp="$(curl -LRsS http://checkip.amazonaws.com/ | \egrep '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')"
+declare vCurrentIp="$(curl -LRsS --connect-timeout 10 http://checkip.amazonaws.com/ | \egrep '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')"
 declare vFile="${HOME:?}/${vGroupId}"
 #
 if [[ "$(< ${vFile})" != "${vCurrentIp:?}" ]]
