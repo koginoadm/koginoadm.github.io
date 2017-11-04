2ace965d632b685a16924032a413bb5ce15367ee
koginoadm <31156607+koginoadm@users.noreply.github.com>
2017-11-05 05:07:05 +0900

    Update aws_sg_update.sh

--- bash/aws_sg_update.sh
+++ bash/aws_sg_update.sh
@@ -20,7 +20,7 @@ then
     then
         #aws ec2 revoke-security-group-ingress --group-id ${vGroupId} --protocol tcp --port 22 --cidr $(< ${vFile})/32
         aws ec2 revoke-security-group-ingress --group-id ${vGroupId} \
-            --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "'"$(< ${vFile})"'/32", "Description": "MyOffice"}]}]'
+            --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "'"$(< ${vFile})"'/32", "Description": "Dynamic"}]}]'
     fi
     #
     printf "${vCurrentIp}" > "${vFile}"
@@ -28,18 +28,18 @@ then
     while (! (aws ec2 describe-security-groups --group-id ${vGroupId} | \grep --quiet $(< ${vFile})))
     do
         aws ec2 authorize-security-group-ingress --group-id ${vGroupId} \
-            --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "'"$(< ${vFile})"'/32", "Description": "MyOffice"}]}]'
+            --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "'"$(< ${vFile})"'/32", "Description": "Dynamic"}]}]'
         i=$((i+1))
         sleep 10
         if [[ $i -gt 3 ]]
         then
             mailbash koginoadm@outlook.com "[AWS][SG][d1.djeeno.net] Failed to update IP Address." \
-                '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "'"$(< ${vFile})"'/32", "Description": "MyOffice"}]}]'
+                '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "'"$(< ${vFile})"'/32", "Description": "Dynamic"}]}]'
             break
         fi
     done
     mailbash koginoadm@outlook.com "[AWS][SG][d1.djeeno.net] IP Address was updated." \
-        '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "'"$(< ${vFile})"'/32", "Description": "MyOffice"}]}]'
+        '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "'"$(< ${vFile})"'/32", "Description": "Dynamic"}]}]'
 fi
 #
 
