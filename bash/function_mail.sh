00b03a09076c2560c0014cabe898ea07abb6d495
koginoadm <31156607+koginoadm@users.noreply.github.com>
2017-11-29 12:00:36 +0900

    Update function_mail.sh

--- bash/function_mail.sh
+++ bash/function_mail.sh
@@ -5,7 +5,7 @@ function mailbash() {
     # To Address
     declare vToAddress="${1:?'$1: Set Mail ToAddress'}"
     #[[ ${vToAddress} =~ [0-9a-zA-Z_\-\.]+@[0-9a-zA-Z_\-\.]+ ]] || { echo '$1: Not a valid RFC-5321 address'; return 1; }
-    (\egrep --quiet "[0-9a-zA-Z_\-\.]+@[0-9a-zA-Z_\-\.]+" <<<"${vToAddress}") || { echo '$1: Not a valid RFC-5321 address'; return 1; }
+    (egrep --quiet "[0-9a-zA-Z_\-\.]+@[0-9a-zA-Z_\-\.]+" <<<"${vToAddress}") || { echo '$1: Not a valid RFC-5321 address'; return 1; }
     declare vSubject="${2:?'$2: Set Mail Subject'}"
     declare vBody="${3:?'$3: Set Mail Body'}"
     # SMTP-AUTH
